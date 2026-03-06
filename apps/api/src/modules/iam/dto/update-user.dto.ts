36ab9fca1832dbab7c4e63287c465efeab291aff
Itallo Neves <nevesautopecasbr@gmail.com>
2026-03-06 17:52:50 -0300

    modulo 01

--- apps/api/src/modules/iam/dto/update-user.dto.ts
+++ apps/api/src/modules/iam/dto/update-user.dto.ts
@@ -1,11 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsOptional, IsUUID } from "class-validator";
 import { CreateUserDto } from "./create-user.dto";
 
-export class UpdateUserDto extends PartialType(
-  Omit<CreateUserDto, "roleId">,
-) {
-  @IsOptional()
-  @IsUUID("4")
-  roleId?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
